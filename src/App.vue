30f64b1a1ec17dce95a21cc7420989cd1de97a2a
Maksim_Khromtsov <maks.fomin.xd@mail.ru>
2024-03-08 16:50:59 +0300

    ccccc

--- src/App.vue
+++ src/App.vue
@@ -1,140 +1,140 @@
 <template>
   <v-app>
-    <div class="w-4/5 m-auto flex flex-column gap-y-5 mt-5 h-4/5">
+<!--    <div class="w-4/5 m-auto flex flex-column gap-y-5 mt-5 h-4/5">-->
 
-      <!-- Выбор типа уведомления -->
-      <div class="flex flex-row gap-x-5">
-        <v-select clearable label="Тип уведомления" :items="selectNotificationTypes" item-value="notificationType" item-title="text"
-          v-model="selectedNotificationType" :hide-details="true">
-        </v-select>
-      </div>
+<!--      &lt;!&ndash; Выбор типа уведомления &ndash;&gt;-->
+<!--      <div class="flex flex-row gap-x-5">-->
+<!--        <v-select clearable label="Тип уведомления" :items="selectNotificationTypes" item-value="notificationType" item-title="text"-->
+<!--          v-model="selectedNotificationType" :hide-details="true">-->
+<!--        </v-select>-->
+<!--      </div>-->
 
-      <!-- Одиночное уведомление -->
-      <div v-if="selectedNotificationType === NotificationType.Single" class="flex flex-row gap-x-5">
-        <h3 class="m-auto">Дата:</h3>
-        <VueDatePicker v-model="singleDates" multi-dates />
-      </div>
+<!--      &lt;!&ndash; Одиночное уведомление &ndash;&gt;-->
+<!--      <div v-if="selectedNotificationType === NotificationType.Single" class="flex flex-row gap-x-5">-->
+<!--        <h3 class="m-auto">Дата:</h3>-->
+<!--        <VueDatePicker v-model="singleDates" multi-dates />-->
+<!--      </div>-->
 
-      <!-- Повторяющееся уведомление -->
-      <div v-if="selectedNotificationType === NotificationType.Repeatable" class="flex flex-column gap-y-5">
+<!--      &lt;!&ndash; Повторяющееся уведомление &ndash;&gt;-->
+<!--      <div v-if="selectedNotificationType === NotificationType.Repeatable" class="flex flex-column gap-y-5">-->
 
-        <div class="flex flex-row gap-x-5">
-          <h3 class="m-auto">Даты:</h3>
-          <VueDatePicker v-model="dateRange" :range="true" :enable-time-picker="false" />
-        </div>
+<!--        <div class="flex flex-row gap-x-5">-->
+<!--          <h3 class="m-auto">Даты:</h3>-->
+<!--          <VueDatePicker v-model="dateRange" :range="true" :enable-time-picker="false" />-->
+<!--        </div>-->
 
-        <!-- Выбор типа интервала недели -->
-        <v-select clearable label="Тип" :items="selectIntervals" item-value="intervalType" item-title="text"
-          v-model="selectedInterval" :hide-details="true">
-        </v-select>
+<!--        &lt;!&ndash; Выбор типа интервала недели &ndash;&gt;-->
+<!--        <v-select clearable label="Тип" :items="selectIntervals" item-value="intervalType" item-title="text"-->
+<!--          v-model="selectedInterval" :hide-details="true">-->
+<!--        </v-select>-->
 
-        <div v-if="selectedInterval === Intervals.Minutes || selectedInterval === Intervals.Hours"
-          class="flex flex-column gap-y-5">
+<!--        <div v-if="selectedInterval === Intervals.Minutes || selectedInterval === Intervals.Hours"-->
+<!--          class="flex flex-column gap-y-5">-->
 
-          <!-- Начальное время -->
-          <div class="flex flex-row gap-x-5">
-            <h3 class="m-auto">Начальное&nbsp;время:</h3>
-            <VueDatePicker v-model="dateFrom" time-picker />
-          </div>
+<!--          &lt;!&ndash; Начальное время &ndash;&gt;-->
+<!--          <div class="flex flex-row gap-x-5">-->
+<!--            <h3 class="m-auto">Начальное&nbsp;время:</h3>-->
+<!--            <VueDatePicker v-model="dateFrom" time-picker />-->
+<!--          </div>-->
 
-          <!-- Каждый интервал -->
-          <div class="flex flex-row gap-x-5">
-            <h3 class="m-auto">Каждые:</h3>
-            <v-text-field hide-details single-line type="number" v-model="numberInterval"></v-text-field>
-          </div>
+<!--          &lt;!&ndash; Каждый интервал &ndash;&gt;-->
+<!--          <div class="flex flex-row gap-x-5">-->
+<!--            <h3 class="m-auto">Каждые:</h3>-->
+<!--            <v-text-field hide-details single-line type="number" v-model="numberInterval"></v-text-field>-->
+<!--          </div>-->
 
-        </div>
+<!--        </div>-->
 
-        <!-- Повторять каждые дни -->
-        <v-radio-group v-if="selectedInterval === Intervals.Days" v-model="chosenRadio" :hide-details="true">
-          <v-radio v-for="(repeatType) in daysRepeatTypes" :key="repeatType.daysRepeatType" :label="repeatType.text" :value="repeatType.daysRepeatType">
-          </v-radio>
-        </v-radio-group>
+<!--        &lt;!&ndash; Повторять каждые дни &ndash;&gt;-->
+<!--        <v-radio-group v-if="selectedInterval === Intervals.Days" v-model="chosenRadio" :hide-details="true">-->
+<!--          <v-radio v-for="(repeatType) in daysRepeatTypes" :key="repeatType.daysRepeatType" :label="repeatType.text" :value="repeatType.daysRepeatType">-->
+<!--          </v-radio>-->
+<!--        </v-radio-group>-->
 
-        <!-- Выбор дня недели -->
-        <v-select v-if="selectedInterval === Intervals.Weeks" :items="selectWeekDays" item-value="weekDayType" item-title="text"
-          label="День" v-model="selectedWeekDays" :hide-details="true" chips multiple>
-        </v-select>
+<!--        &lt;!&ndash; Выбор дня недели &ndash;&gt;-->
+<!--        <v-select v-if="selectedInterval === Intervals.Weeks" :items="selectWeekDays" item-value="weekDayType" item-title="text"-->
+<!--          label="День" v-model="selectedWeekDays" :hide-details="true" chips multiple>-->
+<!--        </v-select>-->
 
-        <div v-if="selectedInterval === Intervals.Months" class="flex flex-column gap-y-5">
+<!--        <div v-if="selectedInterval === Intervals.Months" class="flex flex-column gap-y-5">-->
 
-          <!-- Каждый интервал месяцев -->
-          <div class="flex flex-row gap-x-5">
-            <h3 class="m-auto">Каждые:</h3>
-            <v-select clearable :items="[1, 2, 3, 4, 6]" v-model="dest" :hide-details="true"></v-select>
-          </div>
+<!--          &lt;!&ndash; Каждый интервал месяцев &ndash;&gt;-->
+<!--          <div class="flex flex-row gap-x-5">-->
+<!--            <h3 class="m-auto">Каждые:</h3>-->
+<!--            <v-select clearable :items="[1, 2, 3, 4, 6]" v-model="dest" :hide-details="true"></v-select>-->
+<!--          </div>-->
 
-          <!-- Номер дня месяца -->
-          <div class="flex flex-row gap-x-5">
-            <h3 class="m-auto">День:</h3>
-            <v-text-field hide-details single-line type="number" v-model="numberInterval"></v-text-field>
-          </div>
+<!--          &lt;!&ndash; Номер дня месяца &ndash;&gt;-->
+<!--          <div class="flex flex-row gap-x-5">-->
+<!--            <h3 class="m-auto">День:</h3>-->
+<!--            <v-text-field hide-details single-line type="number" v-model="numberInterval"></v-text-field>-->
+<!--          </div>-->
 
-        </div>
+<!--        </div>-->
 
-        <div v-if="selectedInterval === Intervals.Years" class="flex flex-column gap-y-5">
+<!--        <div v-if="selectedInterval === Intervals.Years" class="flex flex-column gap-y-5">-->
 
-          <!-- Каждые месяцы -->
-          <div class="flex flex-row gap-x-5">
-            <h3 class="m-auto">Каждый:</h3>
-            <v-select :items="selectMonths" item-value="monthType" item-title="text" 
-              v-model="selectedMonth" :hide-details="true" clearable>
-            </v-select>
-          </div>
-          
-          <!-- Номер дня месяца -->
-          <div class="flex flex-row gap-x-5">
-            <h3 class="m-auto">День:</h3>
-            <v-text-field hide-details single-line type="number" v-model="numberInterval"></v-text-field>
-          </div>
+<!--          &lt;!&ndash; Каждые месяцы &ndash;&gt;-->
+<!--          <div class="flex flex-row gap-x-5">-->
+<!--            <h3 class="m-auto">Каждый:</h3>-->
+<!--            <v-select :items="selectMonths" item-value="monthType" item-title="text" -->
+<!--              v-model="selectedMonth" :hide-details="true" clearable>-->
+<!--            </v-select>-->
+<!--          </div>-->
+<!--          -->
+<!--          &lt;!&ndash; Номер дня месяца &ndash;&gt;-->
+<!--          <div class="flex flex-row gap-x-5">-->
+<!--            <h3 class="m-auto">День:</h3>-->
+<!--            <v-text-field hide-details single-line type="number" v-model="numberInterval"></v-text-field>-->
+<!--          </div>-->
 
-        </div>
-        
-      </div>
+<!--        </div>-->
+<!--        -->
+<!--      </div>-->
 
-      <div v-if="selectedInterval === Intervals.Days || selectedInterval === Intervals.Weeks ||
-        selectedInterval === Intervals.Months || selectedInterval === Intervals.Years"
-        class="flex flex-column gap-y-5">
-        <v-list v-if="times.length">
-          <v-list-item class="flex flex-row gap-x-5" v-for="(time, index) in times" :key="index">
-            <div class="flex flex-row gap-x-5">
-              <v-list-item-title class="m-auto">{{ time }}</v-list-item-title>
-              <v-list-item-action>
-                <v-btn @click="removeTime(index)">Удалить</v-btn>
-              </v-list-item-action>
-            </div>
-          </v-list-item>
-        </v-list>
+<!--      <div v-if="selectedInterval === Intervals.Days || selectedInterval === Intervals.Weeks ||-->
+<!--        selectedInterval === Intervals.Months || selectedInterval === Intervals.Years"-->
+<!--        class="flex flex-column gap-y-5">-->
+<!--        <v-list v-if="times.length">-->
+<!--          <v-list-item class="flex flex-row gap-x-5" v-for="(time, index) in times" :key="index">-->
+<!--            <div class="flex flex-row gap-x-5">-->
+<!--              <v-list-item-title class="m-auto">{{ time }}</v-list-item-title>-->
+<!--              <v-list-item-action>-->
+<!--                <v-btn @click="removeTime(index)">Удалить</v-btn>-->
+<!--              </v-list-item-action>-->
+<!--            </div>-->
+<!--          </v-list-item>-->
+<!--        </v-list>-->
 
-        <VueDatePicker placeholder="Добавить время" v-model="selectedTime" @update:model-value="addTime()" time-picker />
-      </div>
+<!--        <VueDatePicker placeholder="Добавить время" v-model="selectedTime" @update:model-value="addTime()" time-picker />-->
+<!--      </div>-->
 
-      <!-- Текст задачи -->
-      <v-textarea clearable label="Задача" variant="solo-inverted" v-model="notificationText"></v-textarea>
+<!--      &lt;!&ndash; Текст задачи &ndash;&gt;-->
+<!--      <v-textarea clearable label="Задача" variant="solo-inverted" v-model="notificationText"></v-textarea>-->
 
-      <v-btn @click="CreateSingleNotification()">Отправить</v-btn>
-      <v-btn @click="s()">2</v-btn>
+<!--      <v-btn @click="CreateSingleNotification()">Отправить</v-btn>-->
+<!--      <v-btn @click="s()">2</v-btn>-->
 
-      <!-- <v-bottom-navigation v-model="value">
-    <v-btn value="recent">
-      <span>Recent</span>
+<!--      &lt;!&ndash; <v-bottom-navigation v-model="value">-->
+<!--    <v-btn value="recent">-->
+<!--      <span>Recent</span>-->
 
-      <v-icon>mdi-history</v-icon>
-    </v-btn>
+<!--      <v-icon>mdi-history</v-icon>-->
+<!--    </v-btn>-->
 
-    <v-btn value="favorites">
-      <span>Favorites</span>
+<!--    <v-btn value="favorites">-->
+<!--      <span>Favorites</span>-->
 
-      <v-icon>mdi-heart</v-icon>
-    </v-btn>
+<!--      <v-icon>mdi-heart</v-icon>-->
+<!--    </v-btn>-->
 
-    <v-btn value="nearby">
-      <span>Nearby</span>
+<!--    <v-btn value="nearby">-->
+<!--      <span>Nearby</span>-->
 
-      <v-icon>mdi-map-marker</v-icon>
-    </v-btn> -->
-      <!-- </v-bottom-navigation> -->
-    </div>
+<!--      <v-icon>mdi-map-marker</v-icon>-->
+<!--    </v-btn> &ndash;&gt;-->
+<!--      &lt;!&ndash; </v-bottom-navigation> &ndash;&gt;-->
+<!--    </div>-->
 
   </v-app>
 </template>
