5b423279d014ff4d09fc8818eaeabc9fc99bfdf5
Maksim_Khromtsov <maks.fomin.xd@mail.ru>
2024-03-09 11:14:13 +0300

    sdds

--- src/App.vue
+++ src/App.vue
@@ -229,7 +229,7 @@ onMounted(async () => {
   var WebApp = window.Telegram.WebApp;
   
   notificationText.value += "\n"
-  notificationText.value += WebApp.WebAppUser.username
+  notificationText.value += window.Telegram.WebAppUser.username
 
   WebApp.showAlert(`Добро пожаловать, @${WebApp.WebAppUser.username}.`);
 })
